818505844405fe5c82179068fe8d3aacb72bd78a
silvideastora <silvideastora18@gmail.com>
2020-12-07 23:52:31 -0600

    ADD First commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+console.log('Hola Koders')
+
+
+
+
+
+
+const NAME_KODER = 'oscar'
+
+let name_koder = prompt('¿Cuál es tu nombre?')
+
+
+
+
+
+console.log(name_koder)
+
+let age_koder = prompt('¿Cuál es tu edad')
+
