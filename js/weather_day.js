f7d2787cda4fc835d612d6fc7017691c910c3ab7
silvideastora <silvideastora18@gmail.com>
2020-12-08 21:40:51 -0600

    ADD Sentencias Switch

--- js/weather_day.js
+++ js/weather_day.js
@@ -1,6 +1,7 @@
 
 const WEATHER = prompt('¿Cómo está el clima?')
-let opciona = 'soleado'
+
+
 
 if(WEATHER === 'soleado' ) {
     console.log('oh!, que buen clima para estar en la playa')
@@ -15,3 +16,25 @@ if(WEATHER === 'soleado' ) {
     const WEATHER_DESCRIPTION = prompt('¿Describe tu dia?')
     console.log(WEATHER_DESCRIPTION)
 }
+
+
+switch(WEATHER){
+    case 'soleado': 
+        console.log('oh!, que buen clima para estar en la playa')
+    break;
+    case 'soleado': 
+        console.log('oh!, que buen clima para estar en la playa')
+    break;
+    case 'soleado': 
+        console.log('oh!, que buen clima para estar en la playa')
+    break;
+    case 'soleado': 
+        console.log('oh!, que buen clima para estar en la playa')
+    break;
+    default:
+        const WEATHER_DESCRIPTION = prompt('¿Describe tu dia?')
+        console.log(WEATHER_DESCRIPTION)
+
+
+}
+
