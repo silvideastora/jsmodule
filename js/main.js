3fa4c28e192fdb80161de5cc6c135d8d7ead3e16
silvideastora <silvideastora18@gmail.com>
2020-12-08 19:52:26 -0600

    ADD Weight in the moon

--- js/main.js
+++ js/main.js
@@ -5,15 +5,15 @@ console.log('Hola Koders')
 
 
 
-const NAME_KODER = 'oscar'
+//const NAME_KODER = 'oscar'
 
-let name_koder = prompt('¿Cuál es tu nombre?')
-
-
+//let name_koder = prompt('¿Cuál es tu nombre?')
 
 
 
-console.log(name_koder)
 
-let age_koder = prompt('¿Cuál es tu edad')
+
+//console.log(name_koder)
+
+//let age_koder = prompt('¿Cuál es tu edad')
 
---
+++ js/weight_in_moon.js
@@ -0,0 +1,5 @@
+let weight_in_earth = prompt('¿Cuál es tu peso en kg?')
+let weight_in_moon = (weight_in_earth / 9.81) * 1.622 
+console.log(weight_in_moon)
+alert(weight_in_moon) 
+
